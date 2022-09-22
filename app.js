67630c8c849e5275ff77a06e84f68a41f29eca04
jonjeung8 <jon.jeung@gmail.com>
2022-09-22 11:47:49 -0500

    mobile commit

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 //Hambuerger Icon
-
 const toggleButton = document.getElementsByClassName("navbar__toggle")[0]
 const navLinks = document.getElementsByClassName("navbar__menu")[0]
 
@@ -7,44 +6,7 @@ const navLinks = document.getElementsByClassName("navbar__menu")[0]
 toggleButton.addEventListener('click', () => {
     navLinks.classList.toggle('active')
 })
-// const menu = document.querySelector('#mobile-menu')
-// const menuLinks = document.querySelector('.navbar__menu')
 
-// menu.addEventListener('click', function() {
-//     menu.classList.toggle('is-active');
-//     menuLinks.classList.toggle('active');
-//});
-
-//Form
-// const steps = Array.from(document.querySelectorAll("form .input"));
-// const next = document.querySelectorAll("form .next__button");
-// const form = document.querySelector("form");
-
-// next = forEach((button) => {
-//     button.addEventListener("click", () => {
-//         changeStep("next");
-//     });
-// });
-// form.addEventListener("submit", (e) => {
-//     e.preventDefault();
-//     const inputs = [];
-//     form.querySelectorAll("input").forEach((input) => {
-//         const { name, value } = input;
-//         inputs.push({ name, value });
-//     });
-//     console.log(inputs);
-//     form.reset();
-// });
-// function changeStep(button) {  
-//     let index = 0;  
-//     const active = document.querySelector(".active");  
-//     index = steps.indexOf(active);  
-//     steps[index].classList.remove("active");  
-//     if (button === "next") {  
-//     index++;  
-//     steps[index].classList.add("active");  
-//     }
-// }   
 const steps = Array.from(document.querySelectorAll("form .input"));  
 const nextBtn = document.querySelectorAll("form .next__button");
 const form = document.querySelector("form");  
@@ -54,6 +16,7 @@ nextBtn.forEach((button) => {
     });  
 });  
 
+//Form
 form.addEventListener("submit", (e) => {  
     e.preventDefault();  
     const inputs = [];  
